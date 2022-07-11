dc0f3c865a631446d67981cb0c2ec4537f269daa
Christophe Thibaut <cthibauttof@gmail.com>
2022-07-11 19:12:15 +0200

    fixed bug (hard coded limit), added examples

--- gdb.fs
+++ gdb.fs
@@ -2,11 +2,11 @@
 VARIABLE #DRIVERS
 
 : BIT>VALUE ( b -- n )
-    1 SWAP LSHIFT ;
+  1 SWAP LSHIFT ;
 
  8 BIT>VALUE CONSTANT MAXDRIVER
 16 BIT>VALUE CONSTANT MAXSTOP
-60 8 *       CONSTANT MAXTIME
+60 8 *     CONSTANT MAXTIME
 
 CREATE STOPS   MAXSTOP   CELLS ALLOT 
 CREATE DRIVERS MAXDRIVER CELLS ALLOT
@@ -14,171 +14,171 @@ CREATE ROUTES  MAXDRIVER MAXTIME * CELLS ALLOT
 CREATE CYCLES  MAXDRIVER CELLS ALLOT
 
 : FULL  ( -- n )
-    #DRIVERS @ BIT>VALUE 1- ;
+  #DRIVERS @ BIT>VALUE 1- ;
 
 : GOSSIP ( driver -- gossip )
-    CELLS DRIVERS + ;
+  CELLS DRIVERS + ;
 
 : COMPLETE ( -- b )
-    FULL
-    #DRIVERS @ 0 DO I GOSSIP @ AND LOOP
-    FULL = ;
-        
-: SET+ ( n,set -- set' )
-    SWAP BIT>VALUE OR ;
+  FULL
+  #DRIVERS @ 0 DO I GOSSIP @ AND LOOP
+  FULL = ;
     
+: SET+ ( n,set -- set' )
+  SWAP BIT>VALUE OR ;
+  
 : IN-SET? ( n,set -- b )
-    SWAP BIT>VALUE AND ;
+  SWAP BIT>VALUE AND ;
 
 : .SET ( drivers -- )
-    #DRIVERS @ 0 DO
-        DUP I SWAP IN-SET? IF I . THEN
-    LOOP DROP ;
+  #DRIVERS @ 0 DO
+    DUP I SWAP IN-SET? IF I . THEN
+  LOOP DROP ;
 
 : CLEAR-DRIVERS
-    DRIVERS MAXDRIVER CELLS ERASE ;
+  DRIVERS MAXDRIVER CELLS ERASE ;
 
 : INIT-DRIVERS
-    #DRIVERS @ 0 DO
-        I BIT>VALUE 
-        DRIVERS I CELLS + !
-    LOOP ;
+  #DRIVERS @ 0 DO
+    I BIT>VALUE 
+    DRIVERS I CELLS + !
+  LOOP ;
 
 : CLEAR-STOPS
-    STOPS MAXSTOP CELLS ERASE ;
+  STOPS MAXSTOP CELLS ERASE ;
 
 : CLEAR-ROUTES
-    ROUTES MAXDRIVER MAXTIME * ERASE ;
+  ROUTES MAXDRIVER MAXTIME * ERASE ;
 
 : STOP-ADDR ( stop -- addr )
-    CELLS STOPS + ;
+  CELLS STOPS + ;
 
 : DRIVER-STOP! ( driver,stop -- )
-    STOP-ADDR DUP @
-    ROT SWAP SET+
-    SWAP ! ;
+  STOP-ADDR DUP @
+  ROT SWAP SET+
+  SWAP ! ;
 
 : DRIVER-ADDR ( driver -- addr )
-    CELLS DRIVERS + ;
+  CELLS DRIVERS + ;
 
 : DRIVER-GOSSIP@ ( driver -- set )
-    DRIVER-ADDR @ ;
+  DRIVER-ADDR @ ;
 
 : DRIVER-GOSSIP! ( gossip,driver -- )
-    DRIVER-ADDR ! ;
+  DRIVER-ADDR ! ;
 
 : COLLECT-STOP-GOSSIP ( stop -- gossip )
-    STOP-ADDR @                \ drivers
-    0 SWAP                     \ acc,drivers
-    #DRIVERS @ 0 DO
-        I OVER IN-SET? IF      \ acc,drivers
-            I DRIVER-GOSSIP@   \ acc,drivers,gossip
-            ROT OR SWAP        \ acc',drivers 
-    THEN LOOP DROP ;
+  STOP-ADDR @        \ drivers
+  0 SWAP           \ acc,drivers
+  #DRIVERS @ 0 DO
+    I OVER IN-SET? IF    \ acc,drivers
+      I DRIVER-GOSSIP@   \ acc,drivers,gossip
+      ROT OR SWAP    \ acc',drivers 
+  THEN LOOP DROP ;
 
 : UPDATE-DRIVER-GOSSIP ( gossip,driver -- )
-    DRIVER-ADDR DUP @ 
-    ROT OR SWAP ! ;
+  DRIVER-ADDR DUP @ 
+  ROT OR SWAP ! ;
 
 : UPDATE-DRIVERS ( gossip,drivers -- )
-    #DRIVERS @ 0 DO                  \ gossip,drivers
-        DUP I SWAP IN-SET? IF        \ gossip,drivers
-            OVER I UPDATE-DRIVER-GOSSIP
-        THEN
-    LOOP DROP DROP ;
-            
+  #DRIVERS @ 0 DO          \ gossip,drivers
+    DUP I SWAP IN-SET? IF    \ gossip,drivers
+      OVER I UPDATE-DRIVER-GOSSIP
+    THEN
+  LOOP DROP DROP ;
+      
 : UPDATE-ALL-DRIVERS
-    6 0 DO
-        I STOP-ADDR @
-        I COLLECT-STOP-GOSSIP
-        SWAP UPDATE-DRIVERS
-    LOOP ;
+  MAXSTOP 0 DO
+    I STOP-ADDR @
+    I COLLECT-STOP-GOSSIP
+    SWAP UPDATE-DRIVERS
+  LOOP ;
 
 : DRIVER-STOP ( minute,driver -- stop )
-    DUP CELLS CYCLES + @ SWAP
-    MAXTIME * CELLS ROUTES + -ROT
-    MOD CELLS + @ ;
-    
+  DUP CELLS CYCLES + @ SWAP
+  MAXTIME * CELLS ROUTES + -ROT
+  MOD CELLS + @ ;
+  
 : STORE-DRIVER-STOP ( stop,minute,driver -- )
-    MAXTIME * + CELLS ROUTES + ! ;
+  MAXTIME * + CELLS ROUTES + ! ;
 
 : DRIVERS-STOPS! ( minute - )
-    #DRIVERS @ 0 DO
-        DUP I DRIVER-STOP
-        I SWAP DRIVER-STOP!
-    LOOP DROP ;
+  #DRIVERS @ 0 DO
+    DUP I DRIVER-STOP
+    I SWAP DRIVER-STOP!
+  LOOP DROP ;
 
 : .DRIVERS 
-    #DRIVERS @ 0 DO
-        I . ." ["
-        I CELLS DRIVERS + @ 
-        DUP FULL = IF DROP ." X" ELSE .SET THEN ." ]  "
-    LOOP ;
+  #DRIVERS @ 0 DO
+    I . ." ["
+    I CELLS DRIVERS + @ 
+    DUP FULL = IF DROP ." X" ELSE .SET THEN 8 EMIT ." ]  "
+  LOOP ;
 
 : .STOPS
-    ." STOPS:"
-    #DRIVERS @ 0 DO
-        DUP I DRIVER-STOP .
-    LOOP CR DROP ;
+  ." STOPS:"
+  #DRIVERS @ 0 DO
+    DUP I DRIVER-STOP .
+  LOOP CR DROP ;
 
 : .STOPS@
-    MAXSTOP 0 DO
-        I CELLS STOPS + @ DUP IF 
-            I . ." (" .SET ." )  " 
-        ELSE 
-            DROP 
-        THEN
-    LOOP CR ;
+  MAXSTOP 0 DO
+    I CELLS STOPS + @ DUP IF 
+      I . ." (" .SET 8 EMIT ." )  " 
+    ELSE 
+      DROP 
+    THEN
+  LOOP CR ;
 
 : TRACK-COMPLETE-GOSSIP 
-    INIT-DRIVERS
+  INIT-DRIVERS
+  .DRIVERS CR
+  0 
+  BEGIN
+    ." MINUTE " DUP . CR
+    DUP .STOPS
+    DUP MAXTIME < COMPLETE 0= AND WHILE
+    CLEAR-STOPS
+    DUP DRIVERS-STOPS!
+    .STOPS@
+    UPDATE-ALL-DRIVERS
     .DRIVERS CR
-    0 
-    BEGIN
-        DUP .
-        DUP .STOPS
-        DUP MAXTIME < COMPLETE 0= AND WHILE
-        CLEAR-STOPS
-        DUP DRIVERS-STOPS!
-        .STOPS@
-        UPDATE-ALL-DRIVERS
-        .DRIVERS CR
-        1+
-    REPEAT
-    COMPLETE IF
-        . CR
-    ELSE
-        DROP ." never" CR
-    THEN ;
+    1+
+  REPEAT
+  COMPLETE IF
+    . CR
+  ELSE
+    DROP ." never" CR
+  THEN ;
 
 : SKIP-NON-DIGIT ( -- n )
-    BEGIN KEY DIGIT? 0= WHILE REPEAT ;
+  BEGIN KEY DIGIT? 0= WHILE REPEAT ;
 
 : GET-NUMBER ( -- n )
-    0 SKIP-NON-DIGIT
-    BEGIN
-        SWAP 10 * +
-        KEY DIGIT?
-    0= UNTIL ;
-    
+  0 SKIP-NON-DIGIT
+  BEGIN
+    SWAP 10 * +
+    KEY DIGIT?
+  0= UNTIL ;
+  
 : GET-STOPS ( driver -- )
-    DUP CELLS CYCLES + @
-    0 DO
-        DUP GET-NUMBER I ROT 
-        STORE-DRIVER-STOP
-    LOOP DROP ;
+  DUP CELLS CYCLES + @
+  0 DO
+    DUP GET-NUMBER I ROT 
+    STORE-DRIVER-STOP
+  LOOP DROP ;
 
 : GET-ROUTE ( driver -- )
-    GET-NUMBER
-    OVER CELLS CYCLES + !
-    GET-STOPS ;
+  GET-NUMBER
+  OVER CELLS CYCLES + !
+  GET-STOPS ;
 
 : GET-ROUTES
-    GET-NUMBER
-    DUP #DRIVERS !
-    0 DO
-        I GET-ROUTE
-    LOOP ;
+  GET-NUMBER
+  DUP #DRIVERS !
+  0 DO
+    I GET-ROUTE
+  LOOP ;
 
 GET-ROUTES
 TRACK-COMPLETE-GOSSIP
